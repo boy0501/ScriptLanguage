d77e1ffc95bd8c69e280586950c7876289283c2b
boy0501 <boy0501@naver.com>
2021-06-03 00:46:25 +0900

    버그수정및 gif이미지추가 및 색상변경
    리스트가 선택이 안되어있을때(즐찾갔을때 아무것도없을때) 정보 누르면 버그떄문에 튕기는거 수정

--- Logo.py
+++ Logo.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import gif
 import gfw
 
 
@@ -8,8 +9,16 @@ class Logo:
     def __init__(self):
         self.frame = Frame(gfw.window)
         self.frame.place(x = 100, y = 0)
-        self.image = gfw.image.load('Asset/image/똥터.png')
-        Label(self.frame,image=self.image).pack()
+        self.logo = gfw.image.load('Asset/image/똥터.png')
+
+        Label(self.frame,image=self.logo,background="skyblue2").pack()
+        lbl = gif.ImageLabel(gfw.window)
+        lbl.load('Asset/image/run2.gif')
+        lbl.place(x=10,y=-50)
+        self.byunimg = PhotoImage(file='Asset/image/메인변기.png')
+        self.byunimg = self.byunimg.subsample(20,20)
+        img = Label(gfw.window,image=self.byunimg,bg="skyblue2")
+        img.place(x=230,y=0)
 
 
     def draw(self):
